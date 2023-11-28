81e8f4e22ebca7120aa16bf588bf5f732d82f28b
jhk01007 <75934142+jhk01007@users.noreply.github.com>
2023-11-29 02:39:37 +0900

    오류 수정

--- database/users.sql
+++ database/users.sql
@@ -5,11 +5,20 @@ create table users(
     userId varchar(50) Not null,
     password varchar(255),
     salt varchar(255),
-    nickname varchar(50),
+    name varchar(50),
     phone varchar(20),
     primary key(id),
     unique (userId)
 ) engine = InnoDB;
-
 select * from users;
+select * from pet;
+create table pet(
+    userId varchar(50) Not null,
+    petName varchar(30),
+    breed varchar(30),
+    age int,
+    primary key(userId)
+) engine = InnoDB;
+
 describe users;
+describe pet;
--- route.js
+++ route.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 
 // 회원가입 엔드포인트
 app.post('/signup', (req, res) => {
-    const { userId, password, nickname, phone } = req.body;
+    const { userId, password, name, phone } = req.body;
 
     console.log(userId);
     // 비밀번호 해싱
@@ -21,8 +21,8 @@ app.post('/signup', (req, res) => {
         }
 
         // MySQL에 사용자 정보 저장
-        db.query('INSERT INTO users (userId, password, salt, nickname, phone) VALUES (?, ?, ?, ?, ?)'
-        , [userId, hash, salt, nickname, phone], (error, results) => {
+        db.query('INSERT INTO users (userId, password, salt, name, phone) VALUES (?, ?, ?, ?, ?)'
+        , [userId, hash, salt, name, phone], (error, results) => {
             if (error) {
                 console.error('Error registering user:', error);
                 res.status(500).send('Error registering user');
@@ -36,7 +36,6 @@ app.post('/signup', (req, res) => {
 // 로그인 엔드포인트
 app.post('/login', (req, res) => {
     const { userId, password } = req.body;
-
     // MySQL에서 사용자 정보 가져오기
     db.query('SELECT * FROM users WHERE userId = ?', [userId], (error, results) => {
         if (error) {
@@ -57,8 +56,8 @@ app.post('/login', (req, res) => {
                     if (hash === user.password) {
                         // 비밀번호가 일치하면 토큰 생성
                         console.log('성공');
-                        const token = jwt.sign({ userId: user.userId, username: user.userId }, '!ddfrf$@df$%^F', { expiresIn: '1h' });
-                        res.json({ 'token': token, 'user': results[0].userId });
+                        const token = jwt.sign({ userId: user.userId, password: user.password }, '!ddfrf$@df$%^F', { expiresIn: '1h' });
+                        res.json({ 'token': token, 'userId': results[0].userId, 'name': results[0].name });
                     } else {
                         console.log('비밀번호 오류');
                         res.status(401).send('Invalid password');
@@ -71,7 +70,22 @@ app.post('/login', (req, res) => {
         }
     });
 });
+// 펫 추가 엔드포인트
+app.post('/addpet', (req, res) => {
+    const { userId, petName, breed, age } = req.body;
+    // MySQL에 사용자의 펫정보 저장
+    db.query('INSERT INTO pet (userId, petName, breed, age) VALUES (?, ?, ?, ?)'
+        , [userId, petName, breed, age], (error, results) => {
+            if (error) {
+                console.error('Error add pet:', error);
+                res.status(500).send('Error add pet');
+            } else {
+                console.log('Add pet successfully');
+                res.status(201).send('Add pet successfully');
+            }
+        });
 
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
